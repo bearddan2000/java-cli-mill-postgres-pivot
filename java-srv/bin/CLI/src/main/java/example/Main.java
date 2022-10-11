a1a5fb3183a7f15f3062afef9b227d66a6112cea
bearddan2000 <bearddan2000@yahoo.com>
2022-10-10 21:57:19 -0400

    initial commit

---
+++ java-srv/bin/CLI/src/main/java/example/Main.java
@@ -0,0 +1,18 @@
+package example;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    final String connectionStr = "jdbc:postgresql://db/animal?user=maria&password=pass";
+
+    example.chain.ILink obj = new example.chain.Driver();
+
+    if (obj.hasResource("org.postgresql.Driver", connectionStr)){
+      example.db.print.output.IOutput output = new example.db.print.output.ToConsole();
+      for ( String tbl : new String[]{"\"public\".\"dog\"", "\"public\".\"breedLookup\""
+      , "\"public\".\"colorLookup\"", "\"public\".\"dog_expanded\"", "\"public\".\"breed_count\""})
+        example.db.DBQuery.query(connectionStr, tbl, output);
+    }
+  }
+}
